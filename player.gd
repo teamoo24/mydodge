6276643ba187a488890924ef700e4c842c099b1b
cC.K <dripcoke24@gmail.com>
2025-06-29 17:00:18 +0900

    first commit

---
+++ player.gd
@@ -0,0 +1,51 @@
+extends Area2D
+signal hit
+@export var speed: float = 400
+var screen_size
+
+func _ready():
+	screen_size = get_viewport().size
+	# This function is called when the node is added to the scene.
+	hide()
+	
+func _process(delta):
+	# This function is called every frame. You can use it to update the player's state.
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+
+func _on_body_entered(_body):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
